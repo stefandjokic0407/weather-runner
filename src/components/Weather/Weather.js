901c246e95a43e56e3c87d7c53b2966f327944a9
Makersmarx <bbiehn1@gmail.com>
2020-09-20 17:14:42 -0600

    get weather search working with City and Country

---
+++ src/components/Weather/Weather.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import GetWeather from '../GetWeather/GetWeather';
+import SearchBar from '../SearchBar/SearchBar';
+
+const Weather = () => {
+  const [weather, setWeather] = useState([]);
+  const APIKEY = '';
+
+  async function fetchData(e) {
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    e.preventDefault();
+
+    const apiData = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${APIKEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => data);
+    if (city && country) {
+      setWeather({
+        data: apiData,
+        city: apiData.city,
+        country: apiData.sys.country,
+        description: apiData.weather[0].description,
+        temperature: Math.round((apiData.main.temp * 9) / 5 - 459.67),
+        error: '',
+      });
+    } else {
+      setWeather({
+        data: '',
+        city: '',
+        country: '',
+        description: '',
+        temperature: '',
+        error: alert('Please Type A City And Country'),
+      });
+    }
+  }
+
+  return (
+    <div className="App">
+      <h3>Please Enter City and Country</h3>
+      <SearchBar getWeather={fetchData} />
+      <GetWeather
+        city={weather.city}
+        country={weather.country}
+        description={weather.description}
+        temperature={weather.temperature}
+        error={weather.error}
+      />
+      {console.log(weather.data)}
+    </div>
+  );
+};
+
+export default Weather;
