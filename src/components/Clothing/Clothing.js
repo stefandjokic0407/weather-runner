2653fd7ca565743ca0664cd0fe16cb9bd90082f4
“stefandjokic0407” <stefandjokic0407@gmail.com>
2020-09-21 15:43:12 -0500

    adding icons to show group

--- src/components/Clothing/Clothing.js
+++ src/components/Clothing/Clothing.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import Shirt from './icons/shirt.png';
 import Shorts from './icons/shorts.png';
+import Shoes from './icons/shoes.png';
+import Sunglasses from './icons/sunglasses.png';
+import Water from './icons/water.png';
 
 class Clothing extends Component {
     render() {
-        return <div>
-            <img src={Shirt} alt="Shirt" />
-            <img src={Shorts} alt="Shorts" />
+        return <div className='clothing-box'>
+            <div>
+                <img src={Shirt} alt="Shirt" />
+            </div>
+            <div>
+                <img src={Sunglasses} alt="Sunglasses" />
+            </div>
+            <div>
+                <img src={Shorts} alt="Shorts" />
+            </div>
+            <div>
+                <img src={Shoes} alt="Shoes" />
+            </div>
+            <div>
+                <img src={Water} alt="Water" />
+            </div>
         </div>
     }
 }
