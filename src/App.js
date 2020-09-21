6c43047a0db6a9a12f4bf55ee658becaf26dcc44
vhamilton08 <vhamilton08@gmail.com>
2020-09-21 15:29:22 -0500

    started building loading view

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Loading from './components/Loading';
 import Weather from './components/Weather/Weather';
 
 function App() {
+  const [loading, setLoading] = useState(true)
+
+  const no = () => {
+    setLoading(false)
+  }
+
+  const yes = () => {
+    setLoading(false)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather Runner Placeholder</h1>
-      </header>
-      <Weather />
+      {loading ? <Loading no={no} yes={yes}/> :
+      <div>
+        <header className="App-header">
+          <h1>Weather Runner Placeholder</h1>
+        </header>
+          <Weather/>
+      </div>}
     </div>
   );
 }
-
 export default App;
+     
