9121be2d1fd6d64cb724d2975005a69e12c524b8
“stefandjokic0407” <stefandjokic0407@gmail.com>
2020-09-22 16:40:05 -0600

    final push for night

--- src/components/GetWeather/GetWeather.js
+++ src/components/GetWeather/GetWeather.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Shirt from './icons/shirt.png';
 
 const GetWeather = ({
   description,
@@ -17,11 +16,7 @@ const GetWeather = ({
         </p>
       )}
       <div className="weather">
-        {temperature && (
-          <p>
-            {temperature} ℉ <img src={Shirt} alt="Shirt" />
-          </p>
-        )}
+        {temperature && <p>{temperature} ℉</p>}
         {wind && <p>{wind} mph</p>}
         {description && <p> {description}</p>}
         {error && <p>{error}</p>}
