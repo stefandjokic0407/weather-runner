e4cbe670e8b5c5d6e821ce9cd6b79a89741eb700
“stefandjokic0407” <stefandjokic0407@gmail.com>
2020-09-22 18:04:13 -0500

    added location permission ask on componentdidmount

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,10 +1,14 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Loading.css'
 
 
 const Loading = (props) => {
 const [latitude, setLatitude] = useState(null)
 const [longitude, setLongitude] = useState(null)
+
+useEffect(() => {
+    getLocation()
+},[])
 
 const getLocation = () => {
     if(navigator.geolocation) {
@@ -17,18 +21,21 @@ const getLocation = () => {
 const getCoordinates = (position) => {
     // console.log(position)
     setLatitude(position.coords.latitude)
-    setLongitude(position.coords.longitude) 
+    setLongitude(position.coords.longitude)
+    // apiData()
+    props.yes()
 }
-    
     return (
         <div>
             <h1>{longitude}</h1>
             <h1>{latitude}</h1>
-            <img class='loading-logo' alt='weather-runner-logo' src='https://cdn.discordapp.com/attachments/756212437606203542/756284007917944882/runner-logo.jpg'/>
+            <img className='loading-logo' alt='weather-runner-logo' src='https://cdn.discordapp.com/attachments/756212437606203542/756284007917944882/runner-logo.jpg'/>
             <h1>Allow us to access your location?</h1>
-            <button onClick={getLocation}>get</button>
-            <button onClick={props.yes}>Yes</button>
-            <button onClick={props.no}>No</button>
+            {/* <button onClick={getLocation}>get</button> */}
+            {/* <button onClick={turnCoords}>turn</button> */}
+            {/* <button onClick={props.yes}>Yes</button> */}
+            <button onClick={props.no}>Search my Location</button>
+
         </div>
     )
 }
