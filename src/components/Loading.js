81e93b1b9abcac4f991fc5e8a9720f44643a4985
Makersmarx <bbiehn1@gmail.com>
2020-09-22 13:45:50 -0600

    change g to G on loading comp to match other buttons

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -15,7 +15,7 @@ const Loading = (props) => {
   };
 
   const getCoordinates = (position) => {
-    // console.log(position)
+    // console.log(position);
     setLatitude(position.coords.latitude);
     setLongitude(position.coords.longitude);
   };
@@ -32,7 +32,7 @@ const Loading = (props) => {
         src="https://cdn.discordapp.com/attachments/756212437606203542/756284007917944882/runner-logo.jpg"
       />
       <h1>Allow us to access your location?</h1>
-      <button onClick={getLocation}>get</button>
+      <button onClick={getLocation}>Get</button>
       <button onClick={props.yes}>Yes</button>
       <button onClick={props.no}>No</button>
     </div>
