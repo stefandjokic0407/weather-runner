a62b88c97fa7e57ad1cf9069c2b2c6f9431ef953
“stefandjokic0407” <stefandjokic0407@gmail.com>
2020-09-22 08:43:39 -0600

    fix typo class to className to resolve error

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,39 +1,42 @@
-import React, { useState } from 'react'
-import './Loading.css'
-
+import React, { useState } from 'react';
+import './Loading.css';
 
 const Loading = (props) => {
-const [latitude, setLatitude] = useState(null)
-const [longitude, setLongitude] = useState(null)
-// const [loading, setLoading] = useState(false)
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  // const [loading, setLoading] = useState(false)
 
-const getLocation = () => {
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(getCoordinates)
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(getCoordinates);
     } else {
-        alert('Geolocation is not supported by this browser.')
+      alert('Geolocation is not supported by this browser.');
     }
-}
+  };
 
-const getCoordinates = (position) => {
+  const getCoordinates = (position) => {
     // console.log(position)
-    setLatitude(position.coords.latitude)
-    setLongitude(position.coords.longitude) 
-}
+    setLatitude(position.coords.latitude);
+    setLongitude(position.coords.longitude);
+  };
 
-// const r
+  // const r
 
-    return (
-        <div>
-            <h1>{longitude}</h1>
-            <h1>{latitude}</h1>
-            <img class='loading-logo' alt='weather-runner-logo' src='https://cdn.discordapp.com/attachments/756212437606203542/756284007917944882/runner-logo.jpg'/>
-            <h1>Allow us to access your location?</h1>
-            <button onClick={getLocation}>get</button>
-            <button onClick={props.yes}>Yes</button>
-            <button onClick={props.no}>No</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>{longitude}</h1>
+      <h1>{latitude}</h1>
+      <img
+        className="loading-logo"
+        alt="weather-runner-logo"
+        src="https://cdn.discordapp.com/attachments/756212437606203542/756284007917944882/runner-logo.jpg"
+      />
+      <h1>Allow us to access your location?</h1>
+      <button onClick={getLocation}>get</button>
+      <button onClick={props.yes}>Yes</button>
+      <button onClick={props.no}>No</button>
+    </div>
+  );
+};
 
-export default Loading
+export default Loading;
