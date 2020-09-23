3622c5dbc6324a460cf6c98b10fca3f6a6426322
“stefandjokic0407” <stefandjokic0407@gmail.com>
2020-09-23 09:51:35 -0500

    removed clothing from app.js

--- src/components/Clothing/Clothing.js
+++ src/components/Clothing/Clothing.js
@@ -1,30 +1,14 @@
-import React, { Component } from 'react';
-import Shirt from './icons/shirt.png';
-import Shorts from './icons/shorts.png';
-import Shoes from './icons/shoes.png';
-import Sunglasses from './icons/sunglasses.png';
-import Water from './icons/water.png';
+import React, { useState } from 'react';
 
-class Clothing extends Component {
-    render() {
-        return <div className='clothing-box'>
-            <div>
-                <img src={Shirt} alt="Shirt" />
-            </div>
-            <div>
-                <img src={Sunglasses} alt="Sunglasses" />
-            </div>
-            <div>
-                <img src={Shorts} alt="Shorts" />
-            </div>
-            <div>
-                <img src={Shoes} alt="Shoes" />
-            </div>
-            <div>
-                <img src={Water} alt="Water" />
-            </div>
-        </div>
-    }
+function Clothing(props) {
+    const [isShowing, toggleShow ] = useState(false)
+    const temp = props.temp;
+
+    
+
+    return <div>
+
+    </div>
 }
 
 export default Clothing;
