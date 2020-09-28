f86219611d59da07357004fcf4f618eb57132e63
Alejandro Sanchez <alejandrosanchez9494@gmail.com>
2020-09-28 11:42:43 -0500

    Updated Loading

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Loading from './components/Loading';
+import Loading from './components/Loading/Loading';
 import Weather from './components/Weather/Weather';
 import Footer from './components/Footer/Footer'
 import './App.scss'
