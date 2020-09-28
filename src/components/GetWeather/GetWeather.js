e714eef7bf511729b722c2ccd9bcbc1bc5fb46c2
Makersmarx <bbiehn1@gmail.com>
2020-09-28 17:23:51 -0600

    store for merge

--- src/components/GetWeather/GetWeather.js
+++ src/components/GetWeather/GetWeather.js
@@ -2,8 +2,18 @@ import React from 'react';
 import Clothing from '../Clothing/Clothing';
 import './GetWeather.scss';
 
-const GetWeather = ({ description, city, country, error, temperature, wind, humidity, image, main, name}) => {
-
+const GetWeather = ({
+  description,
+  city,
+  country,
+  error,
+  temperature,
+  wind,
+  humidity,
+  image,
+  main,
+  name,
+}) => {
   return (
     <div>
       {city && country && (
@@ -13,20 +23,25 @@ const GetWeather = ({ description, city, country, error, temperature, wind, humi
       )}
       <div className="weather">
         <div className="city-box">
-      {name && <p>{name}</p>}
-      <img src={`http://openweathermap.org/img/wn/${image}@2x.png`} alt=""/>
+          {name && <p>{name}</p>}
+          <img
+            src={`http://openweathermap.org/img/wn/${image}@2x.png`}
+            alt=""
+          />
         </div>
         <div className="category-box">
-        {temperature && <p className="temp">Temperature: {temperature} ℉ </p>}
-        {wind && <p>Wind: {wind} mph</p>}
-        {humidity && <p className="humidity">Humidity: {humidity}%</p>}
-        {description && <p className="description">Description: {description}</p>}
-        {error && <p>{error}</p>}
+          {temperature && <p className="temp">Temperature: {temperature} ℉ </p>}
+          {wind && <p>Wind: {wind} mph</p>}
+          {humidity && <p className="humidity">Humidity: {humidity}%</p>}
+          {description && (
+            <p className="description">Description: {description}</p>
+          )}
+          {error && <p>{error}</p>}
         </div>
       </div>
       <div className="clothing-box">
-      <h1>Clothing For Your Run</h1>
-      <Clothing temp={temperature} desc={description} main={main}/>
+        <h1>Clothing For Your Run</h1>
+        <Clothing temp={temperature} desc={description} main={main} />
       </div>
     </div>
   );
